27c41e58235704481abfc3d5aac356f80e7530d4
André Schütz <andre@wegtam.com>
2019-10-22 15:23:22 +0200

    14 monoid, semigroup
    Functor example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,5 @@
 import Examples._
-import cats.Monoid
-import cats.Semigroup
+import cats.{Functor, Monoid, Semigroup}
 import cats.instances.int._
 import cats.instances.string._
 import cats.instances.option._
@@ -45,6 +44,25 @@ object Main {
     val m6: Option[Int] = addAll(List(Some(1), None, Some(2)))
     println(m6)
     // Some(3)
+
+    val md7: String = addAll(List("I", " am ", " a ", " sentence."))
+    println(md7)
+    // I am  a  sentence.
+
+    //
+    // Functor
+    //
+    // Option is a Functor
+    val x: Option[Int] = Some(1)
+    val y: Int = 2
+    val m: Int = 2
+    // adding an Int to an Option of Int and multiplying that by another
+    // Int without e.g. `map` looks like follows
+    val f1 = if(x.isDefined) Some((x.get + y) * m) else None
+    // wih `map`
+    val f2 = x.map(a => (a+y) * m)
+    // by using the associative law
+    val f3 = x.map(_ + y).map(_ * m)
 
   }
 
